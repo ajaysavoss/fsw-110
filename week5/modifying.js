41809274b99e7cda84dd687875506dbe2bdadf86
ajaysavoss <ajaysavoss@bryanuniversity.edu>
2021-04-03 02:37:01 -0500

    initial commit

--- week5/modifying.js
+++ week5/modifying.js
@@ -1,14 +1,41 @@
-alert("Click OK if you are not a robot.");
 
-var password1 = document.getElementById("pass1"),
-    password2 = document.getElementById("pass2");
 
-    function validPass(){
-        if (password1.value != password2.value){
-            password2.setCustomValidity("Passwords Do Not Match.")
-        } else{
-            password2.setCustomValidity("Account Successfully Created.")
+document.getElementById("sub").addEventListener("click", display)
+
+function display(){
+    let firstName = document.getElementById("first")
+    let lastName = document.getElementById("last")
+    let age = document.getElementById("age")
+    let male = document.getElementById("male")
+    let female = document.getElementById("female")
+    let location = document.getElementById("location")
+    let restrict1 = document.getElementById("d1")
+    let restrict2 = document.getElementById("d2")
+    let restrict3 = document.getElementById("d3")
+
+    let gender ;
+        if (male.checked == true){
+            gender = 'male';
+        } else if (female.checked ==true){
+            gender = "female";
         }
-    }
+    
+    let restrictions = "";
+        if (restrict1.checked == true){
+            restrictions = restrictions.concat(restrict1.value).concat(" ");
+        }
+        if (restrict2.checked == true){
+            restrictions = restrictions.concat(restrict2.value).concat(" ");
+        }
+        if (restrict3.checked == true){
+            restrictions = restrictions.concat(restrict3.value).concat(" ");
+        }
+    
 
-    password2.addEventListener("keyup",validPass);
+    alert("First name: " + firstName.value + "\n" + 
+            "Last name: " + lastName.value + "\n" +
+            "Age: " + age.value + "\n" + 
+            "Gender: " + gender + "\n" +
+            "Location: " + location.value + "\n" +
+            "Dietary Restrictions: " + restrictions + "\n");
+}
